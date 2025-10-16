0195fdec3cfe6bd2df2077614fee892477a8b522
rodr1to <rovalverdea@gmail.com>
2025-10-16 01:22:15 -0500

    quick fix to company-admin crud

--- app/Http/Requests/UpdateCompanyRequest.php
+++ app/Http/Requests/UpdateCompanyRequest.php
@@ -3,13 +3,17 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule; // 👈 THIS IS THE MISSING LINE
 
 class UpdateCompanyRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool { return true; }
+    public function authorize(): bool
+    {
+        return true;
+    }
 
     /**
      * Get the validation rules that apply to the request.
