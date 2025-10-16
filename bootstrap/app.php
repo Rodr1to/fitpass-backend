3da7a47ae41f8c8566f817b270794697d96c243f
rodr1to <rovalverdea@gmail.com>
2025-10-16 00:49:53 -0500

    fixes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,14 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias([
-            'admin' => \App\Http\Middleware\EnsureUserHasAdminRole::class,
-        ]);
-    })
+    $middleware->api(prepend: [
+        \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+    ]);
+
+    $middleware->alias([
+        'admin' => \App\Http\Middleware\EnsureUserHasAdminRole::class,
+    ]);
+})
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
