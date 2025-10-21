b9550c63537f0efd7dfc551f3b77ac99b5a9fc74
rodr1to <rovalverdea@gmail.com>
2025-10-21 11:28:59 -0500

    added classmodelfactory data for testing purposes

--- database/factories/ClassModelFactory.php
+++ database/factories/ClassModelFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Partner; // Import the Partner model
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ClassModel>
@@ -16,8 +17,16 @@ class ClassModelFactory extends Factory
      */
     public function definition(): array
     {
+        $startTime = $this->faker->dateTimeBetween('+1 day', '+1 week');
+        $endTime = (clone $startTime)->modify('+1 hour');
+
         return [
-            //
+            'partner_id' => Partner::factory(), // Associate with a Partner
+            'name' => $this->faker->randomElement(['Morning Yoga', 'HIIT Blast', 'Spin Cycle', 'Advanced CrossFit', 'Zumba Party']),
+            'description' => $this->faker->paragraph(3),
+            'start_time' => $startTime,
+            'end_time' => $endTime,
+            'capacity' => $this->faker->numberBetween(10, 30),
         ];
     }
 }
