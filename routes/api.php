903ec57436f69ae5a359a5607984076be4907d94
rodr1to <rovalverdea@gmail.com>
2025-10-21 12:31:10 -0500

    feat(api): modified api for my-bookings route

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\MembershipPlanController;
 use App\Http\Controllers\Api\V1\PartnerController;
+use App\Http\Controllers\Api\V1\BookingController; 
 
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-*/
+/* ... */
 
 // API Version 1 Group
 Route::prefix('v1')->group(function () {
@@ -26,6 +22,6 @@ Route::prefix('v1')->group(function () {
             return $request->user();
         });
 
-        // We will add the booking endpoints inside this group.
+        Route::get('/my-bookings', [BookingController::class, 'index']); // 👈 Add this line
     });
 });
