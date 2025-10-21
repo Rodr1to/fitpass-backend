d9a46dabf45b5e910d8f40c930353002c54be189
rodr1to <rovalverdea@gmail.com>
2025-10-21 12:30:01 -0500

    feat (api): set up authenticating groups and built my bookings endpoint

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\MembershipPlanController;
 use App\Http\Controllers\Api\V1\PartnerController;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,14 +14,18 @@ use App\Http\Controllers\Api\V1\PartnerController;
 
 // API Version 1 Group
 Route::prefix('v1')->group(function () {
+    // --- Public Routes ---
     Route::get('/membership-plans', [MembershipPlanController::class, 'index']);
-
     Route::get('/partners', [PartnerController::class, 'index']);
     Route::get('/partners/{partner}', [PartnerController::class, 'show']);
     Route::get('/partners/{partner}/classes', [PartnerController::class, 'classes']);
-});
 
-// This default route is for Sanctum authentication.
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    // --- Authenticated Routes ---
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+
+        // We will add the booking endpoints inside this group.
+    });
 });
